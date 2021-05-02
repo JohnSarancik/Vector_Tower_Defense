63bec21af2106b77acd321f0fcd23256d19dfaac
JohnSarancik <57106195+JohnSarancik@users.noreply.github.com>
2021-05-02 14:34:29 -0700

    Add files via upload

---
+++ Enemy_Action.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Action : MonoBehaviour
+{
+   
+    public float enemy_move_speed = 0.04f;
+
+    public Transform target;
+    public Transform[] waypoints;
+
+    public int current_waypoint = 0;
+
+    public GameObject enemy;
+    public GameObject spawnpoint;
+
+
+    public void Update(){
+
+        this.transform.position = Vector3.MoveTowards(this.transform.position, waypoints[current_waypoint].position,  enemy_move_speed / 10);
+
+        if(Vector3.Distance(this.transform.position, waypoints[current_waypoint].position) < 0.001f){
+            current_waypoint++;
+        }
+
+        if(current_waypoint == waypoints.Length){
+            current_waypoint = 0;
+            this.transform.position = spawnpoint.transform.position;
+        }
+    }
+}
---
+++ Turret_Action.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_Action : MonoBehaviour
+{
+   public Transform target;
+
+   public GameObject[] enemies;
+
+   public void Update(){
+
+      enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+      target = enemies[0].transform;
+
+       if(Vector3.Distance(this.transform.position, target.position) < 20f){
+        this.transform.LookAt(target);
+        Debug.DrawLine(this.transform.position, target.position, Color.red);
+        Debug.Log("in range");
+       }
+       else{
+           Debug.Log("out of range");
+           return;
+        }
+
+   }
+}
